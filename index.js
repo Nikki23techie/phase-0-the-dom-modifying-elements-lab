9b741ecc9c853dc75ab112bb925e1fd61dfc1654
Manjari Mishra <manjarim5034@gmail.com>
2024-09-10 15:05:25 -0700

    dom-modifying

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+// 1s test. remove main
+const elementBody = document.getElementsByTagName("body")[0];
+const elementMain = document.getElementById("main");
+elementBody.removeChild(elementMain)
+
+//2nd test. newHeader
+const newHeader = document.getElementsByTagName("h1")[0]
+
